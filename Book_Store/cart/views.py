d6535e2aca51266237a526f23baeba6796461257
1000007201 <nishant.sharma5504@gmail.com>
2022-06-12 23:57:06 +0530

    [Nishant] Add:Delete Api view created

--- Book_Store/cart/views.py
+++ Book_Store/cart/views.py
@@ -15,8 +15,8 @@ class AddToCartAPIView(APIView):
             if not type(user_id) == int:
                 return Response(user_id)
             user = user_authenticate(user_id)
-            cart = Cart.objects.get(user=user)
-            serializer = AddToCartSerializer(instance=cart)
+            cart = Cart.objects.filter(user=user)
+            serializer = AddToCartSerializer(instance=cart, many=True)
             return Response({'Data': serializer.data, 'Code': 200})
         except UserNotExist as exception:
             return Response(exception.__dict__)
@@ -29,7 +29,6 @@ class AddToCartAPIView(APIView):
             if not type(user_id) == int:
                 return Response(user_id)
             data = request.data
-            print(data)
             serializer = AddToCartSerializer(data)
             data_dict = dict(serializer.data)
             book_id = data_dict.get('book_id')
@@ -38,7 +37,7 @@ class AddToCartAPIView(APIView):
             book = book_authenticate(book_id, quantity)
             cart = Cart.objects.create(user=user, book=book, quantity=quantity)
             cart.save()
-            return Response({'Message': f'{book.name} added in cart', 'Code': 200})
+            return Response({'Message': f'{book.name} added to cart', 'Code': 200})
         except BookNotExist as exception:
             return Response(exception.__dict__)
         except UserNotExist as exception:
@@ -69,4 +68,18 @@ class UpdateCartApiView(APIView):
         except Exception as e:
             return Response({'Error': str(e), 'Code': 404})
 
+    def delete(self, request, cart_id):
+        try:
+            user_id = token_decode(request)
+            if not type(user_id) == int:
+                return Response(user_id)
+            cart = Cart.objects.get(pk=cart_id)
+            if cart.user.id != user_id:
+                raise UserNotExist('You are not authorised user to make changes', 404)
+            cart.delete()
+            return Response({'Message': 'Cart Deleted', 'Code': 200})
+        except UserNotExist as exception:
+            return Response(exception.__dict__)
+        except Exception as e:
+            return Response({'Error': str(e), 'Code': 404})
 
